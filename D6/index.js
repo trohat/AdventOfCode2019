1fb4220333c7dae6c51c7a456c8af81cd72163c2
Trohat <petr.sed@centrum.cz>
2020-12-07 23:29:03 +0100

    Day 6 & 7

--- D6/index.js
+++ D6/index.js
@@ -1,26 +1,86 @@
 console.log("funguju");
 
+const testdata = `COM)B11
+B11)C11
+C11)D11
+D11)E11
+E11)F11
+B11)G11
+G11)H11
+D11)I11
+E11)J11
+J11)K11
+K11)L11
+K11)YOU
+I11)SAN`;
+
 const splitLines = (data) => data.split(String.fromCharCode(10));
 
-const prepare1 = (data) => data.map((d) => d.split(","));
+const re = /(\w{1,3})\)(\w{1,3})/;
+const prepare = (data) => data.map((d) => {
+    const [ , orbited, orbiting ] = re.exec(d);
+    return { orbited, orbiting };
+});
 
+const orbits = {};
 
-inputdata = prepare2(prepare1(splitLines(inputdata)));
+const task1 = data => {
+    data.forEach( d => {
+        if (!(d.orbited in orbits)) orbits[d.orbited] = {};
+        if (!(d.orbiting in orbits)) orbits[d.orbiting] = {};
+        if ("parent" in orbits[d.orbiting]) console.error("Divné"); else orbits[d.orbiting].parent = d.orbited;
+        if (!("children" in orbits[d.orbited])) orbits[d.orbited].children = [];
+        if (orbits[d.orbited].children.includes(d.orbiting)) console.error("Divné2"); else orbits[d.orbited].children.push(d.orbiting);
+    })
+    /*return Object.keys(orbits).map(key => ({ ...orbits[key], name: key })).sort((a,b) => {
+        if (!("parent" in a)) return -1;
+        if (!("parent" in b)) return 1;
+        return 0 //a.children.length - b.children.length;
+    });*/
+    countNeighbours = (actPlanet, distance) => {
+        const planet = orbits[actPlanet];
+        planet.distance = distance;
+        if (planet.children === undefined) return;
+        for (let i = 0; i < planet.children.length; i++) {
+            countNeighbours(planet.children[i], distance + 1);
+        }
+    }
 
-const data1 = inputdata[0];
-const data2 = inputdata[1];
+    countNeighbours("COM", 0);
 
-countMax(data1);
-countMax(data2);
-const leftDistance = 5000;
-const downDistance = 2000;
+    return Object.keys(orbits).reduce((accumulator, currentValue) => accumulator + orbits[currentValue].distance, 0);
+}
 
+countNeighbours = (actPlanet, distance) => {
+    const planet = orbits[actPlanet];
+    planet.distance = distance;
+    if (planet.children === undefined) return;
+    for (let i = 0; i < planet.children.length; i++) {
+        countNeighbours(planet.children[i], distance + 1);
+    }
+}
 
+const task2 = orbits => {
+    getTravelToBegin = (actPlanet) => {
+        const planet = orbits[actPlanet];
+        if (planet.parent === undefined) return "";
+        return planet.parent + getTravelToBegin(planet.parent);
+    }
 
-let testdata = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`;
+    let san = getTravelToBegin("SAN");
+    let you = getTravelToBegin("YOU");
 
-testdata = prepare2(prepare1(splitLines(testdata)));
+    let go = true;
+    while (go) {
+        if (san.charAt(san.length-1) === you.charAt(you.length-1) ) {
+            san = san.slice(0, san.length-1)
+            you = you.slice(0, you.length-1)
+        } else go = false;
+    }
+    console.log(san, " ", you);
+    return (san.length / 3 ) + (you.length / 3 );
+}
 
-//printField(draw(testdata));
-console.log(task1(draw(inputdata)));
+console.log(task1(prepare(splitLines(inputdata))));
+
+console.log(task2(orbits));
---
+++ D7/index.js
@@ -0,0 +1,158 @@
+console.log("funguju");
+
+const prepare = (program) => {
+  program = program.split(",");
+  program = program.map((d) => +d);
+  return program;
+};
+
+const getAllCombinations = (arr) => {
+  if (arr.length === 1) return [[...arr]];
+  const combinations = [];
+  arr.forEach((d, index) => {
+    let newArr = [...arr];
+    newArr.splice(index, 1);
+    getAllCombinations(newArr).forEach((newD) => {
+      combinations.push([d, ...newD]);
+    });
+  });
+  return combinations;
+};
+
+function* createLoopIterator(parProgram, ampIndex, inputFunction) {
+  const program = [ ... parProgram];
+  let position = 0;
+  const outputs = [];
+
+  mainLoop: while (true) {
+    const getOpcodeAndModes = (instruction) => {
+      opcode = instruction % 100;
+      modes = ((instruction - opcode) / 100).toString().split("");
+      modes = modes.map((m) => +m).reverse();
+    };
+
+    const getArg = (i) => {
+      if (modes[i - 1]) return program[position + i];
+      return program[program[position + i]];
+    };
+
+    let instruction = program[position];
+    let opcode, modes;
+    getOpcodeAndModes(instruction);
+
+    let step, arg1, arg2, storeArg;
+    let useStep = true;
+    switch (opcode) {
+      case 1:
+        arg1 = getArg(1);
+        arg2 = getArg(2);
+        program[program[position + 3]] = arg1 + arg2;
+        step = 4;
+        break;
+      case 2:
+        arg1 = getArg(1);
+        arg2 = getArg(2);
+        program[program[position + 3]] = arg1 * arg2;
+        step = 4;
+        break;
+      case 3:
+        program[program[position + 1]] = inputFunction(ampIndex);
+        step = 2;
+        break;
+      case 4:
+        arg1 = getArg(1);
+        yield arg1;
+        step = 2;
+        break;
+      case 5:
+        arg1 = getArg(1);
+        arg2 = getArg(2);
+        if (arg1) {
+          position = arg2;
+          useStep = false;
+        }
+        step = 3;
+        break;
+      case 6:
+        arg1 = getArg(1);
+        arg2 = getArg(2);
+        if (!arg1) {
+          position = arg2;
+          useStep = false;
+        }
+        step = 3;
+        break;
+      case 7:
+        arg1 = getArg(1);
+        arg2 = getArg(2);
+        if (arg1 < arg2) {
+          storeArg = 1;
+        } else storeArg = 0;
+        program[program[position + 3]] = storeArg;
+        step = 4;
+        break;
+      case 8:
+        arg1 = getArg(1);
+        arg2 = getArg(2);
+        if (arg1 === arg2) {
+          storeArg = 1;
+        } else storeArg = 0;
+        program[program[position + 3]] = storeArg;
+        step = 4;
+        break;
+      case 99:
+        break mainLoop;
+    }
+    if (useStep) position += step;
+  }
+}
+
+const testprogram = prepare(`3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10`);
+
+const findThrusterSignal = (program) => {
+  let maxOutput = 0;
+  let bestComb;
+  let combs = getAllCombinations([5, 6, 7, 8, 9]);
+
+  combs.forEach((comb) => {
+    let combArr = [...comb];
+    let inputIndexArr = [-2, -2, -2, -2, -2];
+    let outputArr = [[0], [], [], [], []];
+    
+    const getInput = (ampIndex) => {
+      inputIndexArr[ampIndex]++;
+      if (inputIndexArr[ampIndex] === -1) {
+        return combArr[ampIndex];
+      } else {
+        return outputArr[ampIndex][inputIndexArr[ampIndex]];
+      }
+    };
+   
+    let generators = [];
+    for (let ampIndex = 0; ampIndex < 5; ampIndex++) {
+      generators.push(createLoopIterator(program, ampIndex, getInput));
+    }
+
+    let generatorIndex = 0;
+    let end = false;
+    while (!end) {
+      let genResult = generators[generatorIndex].next();
+      generatorIndex = (generatorIndex + 1) % 5;
+      outputArr[generatorIndex].push(genResult.value);
+      if (genResult.done && generatorIndex === 0) {
+        end = true;
+      }
+    }
+    outputArr[0].pop();
+    const lastOutput = outputArr[0].pop();
+    if (lastOutput > maxOutput) {
+      maxOutput = lastOutput;
+      bestComb = comb;
+    }
+  });
+  return [maxOutput, bestComb];
+};
+
+console.log(findThrusterSignal(prepare(data)));
