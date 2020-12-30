539121a1e7306e8bbb1a973dee7f931ea811a97f
Trohat <petr.sed@centrum.cz>
2020-12-30 18:25:34 +0100

    Day 22 reducing instructions

--- D22/index.js
+++ D22/index.js
@@ -3,7 +3,7 @@ console.log("funguju");
 const splitLines = (data) => data.split(String.fromCharCode(10));
 
 const prepare = data => {
-    console.log(data.length + " instructions");
+    //console.log(data.length + " instructions");
     data = data.map(instr => {
         if (/new stack/.test(instr)) return { type: "reverse" };
         const incrementRe = /increment (\d+)/;
@@ -28,44 +28,153 @@ const prepare = data => {
     return data;
 };
 
-const task1 = (instructions, task) => {
-    let cards = 10007;
-    if (task === "test") cards = 10;
-    if (task === "task2") cards = 119315717514047;
-    console.log(cards);
-    let deck = new Array(cards);
-    for (let i = 0; i < cards; i++) {
-        deck[i] = i;
+const task1 = (instructions, task, sCard) => {
+    const makeNewInstructions = (instructionsToReduce, count) => {
+      if (instructionsToReduce[0].type === "increment") {
+        //console.log("This should be increment: " + instructionsToReduce[1].type);
+        return [
+          {
+            type: "increment",
+            number: (instructionsToReduce[0].number * instructionsToReduce[1].number) % count,
+          },
+        ];
+      }
+      if (instructionsToReduce[0].type === "cut") {
+        if (instructionsToReduce[1].type === "cut") {
+          return [
+            {
+              type: "cut",
+              number: (instructionsToReduce[0].number + instructionsToReduce[1].number) % count,
+            }
+          ]
+        }
+        //console.log("This should be increment: " + instructionsToReduce[1].type);
+        return [
+          {
+            type: "increment",
+            number: instructionsToReduce[1].number
+          },
+          {
+            type: "cut",
+            number: (instructionsToReduce[0].number * instructionsToReduce[1].number) % count,
+          }
+        ]
+      }
+      //console.log("This should be reverse: " + instructionsToReduce[0].type);
+      if (instructionsToReduce[1].type === "reverse") {
+        return [];
+      }
+      if (instructionsToReduce[1].type === "cut") {
+        return [
+          {
+            type: "cut",
+            number: count - instructionsToReduce[1].number
+          },
+          {
+            type: "reverse"
+          }
+        ];
+      }
+      //console.log("This should be increment: " + instructionsToReduce[1].type);
+      let x = instructionsToReduce[1].number;
+      return  [
+        {
+          type: "increment",
+          number: instructionsToReduce[1].number
+        },
+        {
+          type: "cut",
+          number: 1 - x
+        },
+        {
+          type: "reverse"
+        }
+      ];
+    };
+
+    const reduceInstructions = (instructions, count) => {
+        let ordered = false;
+        const order = { "increment": 0, "cut": 1, "reverse":2 };
+        mainWhile: while (!ordered) {
+            for (let i = 0; i < instructions.length - 1; i++) {
+                if (order[instructions[i].type] >= order[instructions[i+1].type]) {
+
+                    let newInstructions = makeNewInstructions(instructions.splice(i,2), count);
+                    instructions.splice(i, 0, ...newInstructions);
+                    continue mainWhile;
+                }
+            }
+            ordered = true;
+        }
+
+        return instructions;
     }
-    for (const instr of instructions) {
-        let n;
+
+
+    let deck = 10007;
+    let card = 3589;
+    if (task === "test") deck = 10;
+    if (task === "test") card = sCard;
+    if (task === "task2") deck = 119315717514047;
+    if (task === "task2") card = 2020;
+    instructions = reduceInstructions(instructions, deck);
+    
+    const baseInstructions = [ ...instructions ];
+    
+    let finishedInstructions = [];
+    let repetitions = 1;
+    if (task === "task2") repetitions = 101741582076661;
+    while (repetitions > 0) {
+      let many = 1;
+      instructions = [ ...baseInstructions];
+      while ((many*2) <= repetitions) {
+        many *= 2;
+        instructions = reduceInstructions(instructions.concat(instructions), deck);
+      }
+      console.log("Many:", many, " Repetitions:", repetitions, " After subtracting:", repetitions-many);
+      repetitions = repetitions - many;
+      finishedInstructions = finishedInstructions.concat(instructions);
+    }
+    finishedInstructions = reduceInstructions(finishedInstructions, deck);
+    console.log(finishedInstructions);
+    instructions = [ ...finishedInstructions];
+    
+    for (let i = instructions.length - 1; i >= 0; i--) {
+        let instr = instructions[i];
         switch (instr.type) {
           case "reverse":
-            deck.reverse();
+            card = deck - 1 - card;
             break;
           case "increment":
-            n = instr.number;
-            let pos = 0;
-            let newDeck = Array(cards);
-            for (const card of deck) {
-              newDeck[pos] = card;
-              pos = (pos + n) % cards;
+            console.log("starting increment")
+            let inc = instr.number;
+            let step = 0;
+            let steps = 0;
+            while (step !== card) {
+              step += inc;
+              steps++;
+              if (step >= deck) {
+                step -= deck;
+              }
+              //console.log("Card:", card / 10e12, "Inc:", inc / 10e12, "Step:", step / 10e12);
             }
-            deck = [...newDeck];
+            console.log("ending increment")
+            card = steps;
             break;
           case "cut":
-            n = instr.number;
-            if (n < 0) n += cards;
-            let cutDeck = deck.splice(0, n);
-            deck = deck.concat(cutDeck);
+            let cut = instr.number;
+            if (cut < 0) cut += deck;
+            let noCut = deck - cut;
+            if (card < noCut) card = card + cut;
+            else card = card - noCut;
             break;
           default:
             console.error("Unknown action");
         }
     }
-    if (task === "test") return deck;
+    
 
-    return deck.indexOf(2019);
+    return card;
 };
 
 const task2 = data => {
@@ -85,15 +194,13 @@ cut -1`;
 console.time();
 instructions = prepare(splitLines(inputdata));
 
-console.log(instructions);
 
-testinstructions = prepare(splitLines(testdata));
-
-console.log(testinstructions);
+//testinstructions = prepare(splitLines(testdata));
+//console.log(instructions);
 
 console.log("");
 
-doEqualTest(task1(testinstructions, "test"), 2);
+//doEqualTest(task1(testinstructions, "test", 0), 2);
 
 //console.log("Task 1: " + task1(instructions));
 
